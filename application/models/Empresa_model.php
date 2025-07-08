8ffd8771b32cea411f23a15e45078a703117b8aa
Javier Curichimba <javiergym@36hotmail.com>
2025-07-07 22:10:38 -0500

    Sigue correcion empresa

--- application/models/Empresa_model.php
+++ application/models/Empresa_model.php
@@ -10,13 +10,13 @@ class Empresa_model extends CI_Model
 	}
 
 	public function mostrar()
-	{		
-		$query=$this->db
-				->select("id, ruc, nombres, ncomercial, producto, dscto, lstock, pesencial, logo, lticket, lestablecimiento, ticket, pie, piec, time, compra, arqueo, pventa, pestablecimiento, spuntos, pcompra, gunidad, gblister, gcaja, vbonificar, facturacion, tipo_soap, envio_automatico, envio_boleta, envio_guia, usuario_soap, clave_soap, certificado, certificado_clave, certificado_vence, edicion, id_validador, secret_validador, token_validador, fecha_validador, expires_validador, id_gre, secret_gre, token_gre, fecha_gre, expires_gre")
-				->from($this->table)
-				->get();
-		return $query->row();
-	}
+{		
+	$query=$this->db
+			->select("id, ruc, nombres, ncomercial, producto, dscto, pesencial, logo, lticket, lestablecimiento, ticket, pie, piec, time, compra, arqueo, pventa, pestablecimiento, spuntos, pcompra, gunidad, gblister, gcaja, vbonificar, facturacion, tipo_soap, envio_automatico, envio_boleta, envio_guia, usuario_soap, clave_soap, certificado, certificado_clave, certificado_vence, edicion, id_validador, secret_validador, token_validador, fecha_validador, expires_validador, id_gre, secret_gre, token_gre, fecha_gre, expires_gre")
+			->from($this->table)
+			->get();
+	return $query->row();
+}
 
 	public function update($data=array())
 	{
