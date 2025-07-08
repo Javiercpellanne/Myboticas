4b5d35d6c192eb35cd58118571ddbc5c70057424
Javier Curichimba <javiergym@36hotmail.com>
2025-07-07 22:04:56 -0500

    Corrigiendo empresa bd

--- application/models/Empresa_model.php
+++ application/models/Empresa_model.php
@@ -12,7 +12,7 @@ class Empresa_model extends CI_Model
 	public function mostrar()
 	{		
 		$query=$this->db
-				->select("id, ruc, nombres, ncomercial, producto, dscto, lventa, lstock, pesencial, logo, lticket, lestablecimiento, ticket, pie, piec, time, compra, arqueo, pventa, pestablecimiento, spuntos, pcompra, gunidad, gblister, gcaja, vbonificar, facturacion, tipo_soap, envio_automatico, envio_boleta, envio_guia, usuario_soap, clave_soap, certificado, certificado_clave, certificado_vence, edicion, id_validador, secret_validador, token_validador, fecha_validador, expires_validador, id_gre, secret_gre, token_gre, fecha_gre, expires_gre")
+				->select("id, ruc, nombres, ncomercial, producto, dscto, lstock, pesencial, logo, lticket, lestablecimiento, ticket, pie, piec, time, compra, arqueo, pventa, pestablecimiento, spuntos, pcompra, gunidad, gblister, gcaja, vbonificar, facturacion, tipo_soap, envio_automatico, envio_boleta, envio_guia, usuario_soap, clave_soap, certificado, certificado_clave, certificado_vence, edicion, id_validador, secret_validador, token_validador, fecha_validador, expires_validador, id_gre, secret_gre, token_gre, fecha_gre, expires_gre")
 				->from($this->table)
 				->get();
 		return $query->row();
